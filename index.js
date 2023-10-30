f8697a0f15f0743c6b7c053d83ab7231288fcf67
MSimetra <chillonev@gmail.com>
2023-10-30 15:20:21 +0200

    second commit

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 'use stict'
 
+const userService = require('./userService');
 const express = require('express');
 const app = express();
 
 const endpoints = {
   getUser: {
     method: 'get',
-    handler: (req, res, next) => {
-      res.status(200).send('<h1>User found</h1>')
-    }
+    handler: (args) => userService.getUser(args)
   },
   addUser: {
     method: 'post',
-    handler: (req, res, next) => {
-      res.status(200).send('<h1>User added</h1>')
-    }
+    handler: (args) => userService.addUser(args)
   }
 }
 
@@ -22,7 +19,10 @@ const start = () => {
   for (const endpoint in endpoints) {
     const method = endpoints[endpoint].method;
     const resourse = `/${endpoint}`
-    app[method](resourse, endpoints[endpoint].handler)
+    app[method](resourse, (req, res, next) => {
+      const response = endpoints[endpoint].handler(req.body || req.params)
+      res.send(response)
+    })
   }
 
   app.all('*', (req, res) => {
---
+++ userService.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const getUser = (args) => {
+  return '<h1>User found</h1>'
+}
+
+const addUser = (args) => {
+  return '<h1>User added</h1>'
+}
+
+module.exports = { getUser, addUser };
